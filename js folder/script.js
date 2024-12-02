88449ccaa146ed6af40bb4909e38fb180db30d73
Mohd Sahil <mohdsahildw@gmail.com>
2024-12-02 23:59:22 +0530

    js

---
+++ js folder/script.js
@@ -0,0 +1,14 @@
+alert("This is alert");
+
+console.log("console help to see the execution,which can see in inspect")
+console.log("Console output shows in inspect section in the chrome")
+
+var a = prompt("Enter your number")
+console.log("your number is "+a) 
+var isTrue = confirm("DO you like competitive programmming")
+if(isTrue){
+    console.log("Oh! that's good; you want to become a RED CODER")
+}
+else{
+    console.log("Oh! that's good; you want to become a Software Developer")
+}
