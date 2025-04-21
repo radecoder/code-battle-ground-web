e976d1cbc25096eb008991095b6eb9bdbbc71482
radecoder <mohdsahildw@gmail.com>
2025-04-22 01:14:20 +0530

    creating alerts & console log

--- js folder/54 js intro/script.js
+++ js folder/54 js intro/script.js
@@ -1,17 +1,20 @@
 alert("This is alert");
 
-console.log("console help to see the execution,which can see in inspect")
-console.log("Console output shows in inspect section in the chrome")
+console.log("console log ki helps se, we can see the execution,  ")
+console.log("Console ka output shows in inspect section in the chrome")
 
-// var a = prompt("Enter your number")
-// console.log("your number is "+a) 
-// var isTrue = confirm("DO you like competitive programmming")
-// if(isTrue){
-//     console.log("Oh! that's good; you want to become a RED CODER")
-// }
-// else{
-//     console.log("Oh! that's good; you want to become a Software Developer")
-// }
-// document.title = "This is title"
+var a = prompt("Enter your number")
+console.log("your number is "+a) 
+var isTrue = confirm("this prompt is comming using Confirm keyword, to console log s/w dev or coder")
+if(isTrue){
+    console.log("Oh! that's good; you want to become a RED CODER")
+}
+else{
+    console.log("Oh! that's good; you want to become a Software Developer")
+}
 
-// document.body.style.backgroundColor = "green"
+document.title = "nodejs title"
+console.log("with help of node js we can also make title as u can see now & bg also")
+
+document.body.style.backgroundColor = "green"
+console.log("Nodejs also change the background color, img.. as it is currently green")
