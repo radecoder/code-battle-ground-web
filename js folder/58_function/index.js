696f7b06b3da914b11a98b42ac1905394f0dee2c
radecoder <mohdsahildw@gmail.com>
2025-04-24 16:58:18 +0530

    function in js

--- js folder/58_function/index.js
+++ js folder/58_function/index.js
@@ -4,27 +4,68 @@ function nice(name) {
     console.log("Hey " + name + " your tshirt is nice!")
     console.log("Hey " + name + " your course is good too!")
 }
+nice("Mohd Sahil")
 
-function sum(a, b, c = 3) {
-    // console.log(a + b)
-    console.log(a, b, c)
-    return a + b + c
+console.log("")
+
+function sum(a,b,c=6){
+    return a+b+c
 }
+result = sum (12,12)
+console.log("sum is :",result)
+
+console.log("")
 
 
-result1 = sum(3, 2)
-result2 = sum(7, 5)
-result3 = sum(3, 13, 1)
+// function sum(a, b, c = 3) {
+//     // console.log(a + b)
+//     console.log(a, b, c)
+//     return a + b + c
+// }
+
+
+result1 = sum(3, 1)
+result2 = sum(7, 2)
+result3 = sum(3, 13, 4)
 
 console.log("The sum of these numbers is: ", result1)
 console.log("The sum of these numbers is: ", result2)
 console.log("The sum of these numbers is: ", result3)
 
+console.log("")
 
-const func1 = (x)=>{
+const summation =(a,b,c=5) => {
+    return a+b+c;
+
+}
+r1 = summation(5,5)
+console.log(r1)
+console.log("")
+
+
+console.log("Arrow Func- ")
+console.log("convinent way to write fun")
+// here func1 is variable that can be used in other func
+const func1 = (x)=>{  
     console.log("I am an arrow function", x)
 }
 
 func1(34);
 func1(66);
 func1(84);
+
+
+
+
+console.log("")
+
+const Arrow = (x) => {
+    console.log("This is First Arrow Func :",x)
+}
+Arrow(10);
+Arrow(20);
+Arrow(30);
+
+
+
+
---
+++ js folder/58_function/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function sum(a, b, c = 3) {
+//     // console.log(a + b)
+//     console.log(a, b, c)
+//     return a + b + c
+// }
