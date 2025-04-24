32bac5bfc42a0221e86c89bdab68d0190b00e656
radecoder <mohdsahildw@gmail.com>
2025-04-24 17:29:51 +0530

    crating falty calci

---
+++ js folder/61_sol-of59/index.js
@@ -0,0 +1,13 @@
+// const sum = (a,b) => {
+//     return a+b;
+// }
+// r1=sum(1,4)
+
+// console.log(r1)
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+const r1 = sum(1, 4); // or let r1 = sum(1, 4);
+console.log(r1); // Output: 5
