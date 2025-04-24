9e29ce8b3017298f2044f93c77c362b935a44323
radecoder <mohdsahildw@gmail.com>
2025-04-24 23:35:10 +0530

    falci calculator done

--- js folder/59_exercise_falty_clac/index.js
+++ js folder/59_exercise_falty_clac/index.js
@@ -13,3 +13,29 @@ This faulty calculator does following:
 It performs wrong operation 10% of the times
 
 */
+
+console.log("Falti calculator") 
+console.log()
+
+let a =3
+let b =2
+console.log(a,b)
+let c = Math.random() 
+console.log(c)
+
+if (c>0.1){
+    console.log("Addition is:", a+b)
+    console.log("Multiplication is:", a*b)
+    console.log("Subtract is:", a-b)
+    console.log("Division is:", a/b)
+    
+}
+else{
+    console.log("Addition is:", a-b)
+    console.log("Multiplication is:", a+b)
+    console.log("Subtract is:", a/b)
+    console.log("Division is:", a**b)
+    
+
+}
+// console.log()
