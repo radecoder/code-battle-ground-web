7e2a39f41f120c72e2d177210e691493999dba84
radecoder <mohdsahildw@gmail.com>
2025-04-25 17:42:37 +0530

    falty calculator done

--- js folder/61_sol-of59/index.js
+++ js folder/61_sol-of59/index.js
@@ -1,13 +1,35 @@
-// const sum = (a,b) => {
-//     return a+b;
-// }
-// r1=sum(1,4)
+/* Create a faulty calculator using JavaScript
 
-// console.log(r1)
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
 
-const sum = (a, b) => {
-    return a + b;
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+
+It performs wrong operation 10% of the times
+
+*/
+let random = Math.random()
+console.log(random)
+
+let a = prompt("Enter First No.")
+let b = prompt("Enter Operation")
+let c = prompt("Enter Second No.")
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**"
 }
 
-const r1 = sum(1, 4); // or let r1 = sum(1, 4);
-console.log(r1); // Output: 5
+if (random>0.1){
+    alert(`result is ${eval (`${a} ${b} ${c} `)}` ) 
+}
+else{
+    b = obj[b]
+    alert(`result is ${eval (`${a} ${b} ${c} `)}` )
+}
