f6a6464de04051902cb65a7e3ffc04767b92bc56
radecoder <mohdsahildw@gmail.com>
2025-04-25 16:58:35 +0530

    string with its method and functions

--- js folder/60_strings/index.js
+++ js folder/60_strings/index.js
@@ -1,27 +1,46 @@
-console.log("This is strings tutorial")
-let a = "Harry";
-console.log(a[0]);
-console.log(a[1]);
-console.log(a[2]);
-console.log(a[3]);
-console.log(a[4]);
-// console.log(a[5]);
+console.log("This is tutorial for Strings ")
 
+console.log("Mohd Sahil")
+let a = "Mohd Sahil"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
 console.log(a.length)
 
-let real_name = "Harry"
-let friend = "Rohan"
-console.log("His name is " + real_name + " and his friends name is " + friend)
-console.log(`His name is ${real_name} and his friends name is ${friend}`)
+let myname = "Mohd Sahil"
+let frdname = "Sameer Khan"
+console.log("Hey my name is " + myname + " and my friend name is " + frdname)
+//or this    convinent way of writing this into variables
+console.log(`Hey my name is ${myname} and my friend name is ${frdname}`)
 
-let b = "ShivamSh"
-console.log(b.toUpperCase())
-console.log(b.toLowerCase()) 
-console.log(b.length) 
-console.log(b.slice(1, 5)) 
-console.log(b.slice(1)) 
+// String methods and some function
 
-console.log(b.replace("Sh", "77"))
-console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+console.log(myname.toUpperCase())   // uppercae   o/p = MOHD SAHIL
+console.log(myname.toLowerCase())  // lowercase  o/p = mohd sahil
+console.log(myname.length)        // length o/p = 10
+console.log(myname.slice(1, 4))  // Slice  o/p = ohd   1 included and 4 not included
+console.log(myname.slice(1))    // Slice  o/p = ohd Sahil      only  1 included
+console.log(myname.replace("Sahil" , "Coder"))    // Replace  o/p = Mohd Coder     
+console.log(myname.concat(" Rade " , "Coder"))    // Concatinate  o/p = Mohd Sahil Rade Coder    we can also use + operator 
 
-console.log(b)
+
+
+
+// let b = "ShivamSh"
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase()) 
+// console.log(b.length) 
+// console.log(b.slice(1, 5)) 
+// console.log(b.slice(1)) 
+
+// console.log(b.replace("Sh", "77"))
+// console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+
+// console.log(b)
