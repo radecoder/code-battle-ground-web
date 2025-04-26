bf7c84f9988047fdacaf7c04ea0136c81a4666e7
radecoder <mohdsahildw@gmail.com>
2025-04-26 17:27:52 +0530

    array finishing

--- js folder/63_array/index.js
+++ js folder/63_array/index.js
@@ -2,22 +2,50 @@ let arr = [1, 2, 4, 5, 7]
 //  Index  0, 1, 2, 3, 4
 
 arr[0] = 5666;
-console.log(arr, typeof arr);
-console.log(arr.length)
-console.log()
+console.log(arr, typeof arr); //type
+console.log(arr.length)      //length
+console.log("")
+
 console.log(arr[0])
 console.log(arr[2])
 console.log(arr[4])
-console.log()
-console.log(arr.toString())
-console.log(arr.join(" and "))
-console.log(arr.push("Mohd Sahil"))
+console.log("")
 
-console.log(arr.length)
+console.log(arr.toString()) //convert into string
+console.log("")
 
-console.log(arr.unshift("Mohd Sahil"))
-// console.log(arr)
- 
+console.log(arr.join(" and ")) //adding and b/w no.s
+console.log("")
+
+console.log(arr.pop())         // pop out last element
+console.log("")
+
+arr.push("Mohd Sahil")         // push ele in last
+console.log(arr)
+console.log("")
+
+arr.shift()                   //pop 1st element
+console.log(arr)
+console.log("")
+
+arr.unshift("Rade Coder")    // push ele in first
+console.log(arr)
+console.log("")
+
+delete(arr[3])           // delete ele but memory remain
+console.log(arr)
+console.log("")
+
+let a1 = [1, 2, 3]
+let a2 = [14, 12, 6]   //concat to add mutiple arr
+let a3 = [31, 29, 8]   //does not change existing arr
+console.log(a1.concat(a2,a3)) 
+console.log("")
+
+let a4 = [23,10,12]
+a4.sort()           //sort arr & modifies original arr
+console.log(a4)
+
 // let numbers = [1, 2, 3, 4, 5] 
 // numbers.splice(1, 2)    
 // numbers.splice(1, 3)  
